f5fcdfd37aa16f2a33e9451addd2736fd56042b7
Weiwei Yang <weiwei.yang@cloudera.com>
2019-10-22 10:34:02 -0700

    fix various of things for tests

--- pkg/scheduler/sorters_test.go
+++ pkg/scheduler/sorters_test.go
@@ -17,98 +17,161 @@ limitations under the License.
 package scheduler
 
 import (
-	"github.com/cloudera/yunikorn-core/pkg/cache"
 	"github.com/cloudera/yunikorn-core/pkg/common/resources"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
+// verify queue ordering is working
 func TestSortQueues(t *testing.T) {
-	// define queue info without guaranteed resources
-	root   := newQueueInfo("root", nil, 1000, 1000, 1000, 1000)
-	q0info := newQueueInfo("q0", root, 0, 0, 500, 500)
-	q1info := newQueueInfo("q1", root, 0, 0, 200, 200)
-	q2info := newQueueInfo("q2", root, 0, 0, 300, 300)
-	qRoot  := newSchedulingQueue("root", root, nil, 300, 200)
-	q0     := newSchedulingQueue("q0", q0info, qRoot,300, 300)
-	q1     := newSchedulingQueue("q1", q1info, qRoot,200, 200)
-	q2     := newSchedulingQueue("q2", q2info, qRoot, 100, 100)
+	root, err := createRootQueue()
+	root.CachedQueueInfo.GuaranteedResource = resources.NewResourceFromMap(
+		map[string]resources.Quantity{"memory": 1000, "vcore": 1000})
+	if err != nil {
+		t.Fatalf("failed to create basic root queue: %v", err)
+	}
+
+	q0, err := createManagedQueue(root, "q0", false)
+	q0.CachedQueueInfo.GuaranteedResource = resources.NewResourceFromMap(
+		map[string]resources.Quantity{"memory": 500, "vcore": 500})
+	q0.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{
+		"memory": resources.Quantity(300),
+		"vcore" : resources.Quantity(300)})
+	if err != nil {
+		t.Fatalf("failed to create leaf queue: %v", err)
+	}
+
+	q1, err := createManagedQueue(root, "q1", false)
+	q1.CachedQueueInfo.GuaranteedResource = resources.NewResourceFromMap(
+		map[string]resources.Quantity{"memory": 300, "vcore": 300})
+	q1.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{
+		"memory": resources.Quantity(200),
+		"vcore" : resources.Quantity(200)})
+	if err != nil {
+		t.Fatalf("failed to create leaf queue: %v", err)
+	}
+
+	q2, err := createManagedQueue(root, "q2", false)
+	q2.CachedQueueInfo.GuaranteedResource = resources.NewResourceFromMap(
+		map[string]resources.Quantity{"memory": 200, "vcore": 200})
+	q2.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{
+		"memory": resources.Quantity(100),
+		"vcore" : resources.Quantity(100)})
+	if err != nil {
+		t.Fatalf("failed to create leaf queue: %v", err)
+	}
 
 	queues := []*SchedulingQueue{q0, q1, q2}
 	SortQueue(queues, FairSortPolicy)
 	assert.Equal(t, len(queues), 3)
-	assert.Equal(t, queues[0].Name, "q2")
-	assert.Equal(t, queues[1].Name, "q1")
-	assert.Equal(t, queues[2].Name, "q0")
+	assert.Equal(t, "root.q2", queues[0].Name)
+	assert.Equal(t, "root.q1", queues[1].Name)
+	assert.Equal(t, "root.q0", queues[2].Name)
 
 	q0.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{"memory": 200, "vcore": 200})
 	q1.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{"memory": 300, "vcore": 300})
 	SortQueue(queues, FairSortPolicy)
 	assert.Equal(t, len(queues), 3)
-	assert.Equal(t, queues[0].Name, "q2")
-	assert.Equal(t, queues[1].Name, "q0")
-	assert.Equal(t, queues[2].Name, "q1")
+	assert.Equal(t, "root.q2", queues[0].Name)
+	assert.Equal(t, "root.q0", queues[1].Name)
+	assert.Equal(t, "root.q1", queues[2].Name)
 }
 
-func TestNoLimitQueues(t *testing.T) {
-	// define queue info without guaranteed resources
-	root   := newQueueInfo("root", nil, 0, 0, 0, 0)
-	q0info := newQueueInfo("q0", root, 0, 0, 0, 0)
-	q1info := newQueueInfo("q1", root, 0, 0, 0, 0)
-	q2info := newQueueInfo("q2", root, 0, 0, 0, 0)
-	qRoot  := newSchedulingQueue("root", root, nil, 300, 200)
-	q0     := newSchedulingQueue("q0", q0info, qRoot,100, 100)
-	q1     := newSchedulingQueue("q1", q1info, qRoot,200, 100)
-	q2     := newSchedulingQueue("q2", q2info, qRoot, 0, 0)
+// queue guaranteed resource is 0
+func TestNoQueueLimits(t *testing.T) {
+	root, err := createRootQueue()
+	root.CachedQueueInfo.GuaranteedResource = resources.NewResourceFromMap(
+		map[string]resources.Quantity{"memory": 0, "vcore": 0})
+	if err != nil {
+		t.Fatalf("failed to create basic root queue: %v", err)
+	}
+
+	q0, err := createManagedQueue(root, "q0", false)
+	q0.CachedQueueInfo.GuaranteedResource = resources.NewResourceFromMap(
+		map[string]resources.Quantity{"memory": 0, "vcore": 0})
+	q0.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{
+		"memory": resources.Quantity(300),
+		"vcore" : resources.Quantity(300)})
+	if err != nil {
+		t.Fatalf("failed to create leaf queue: %v", err)
+	}
+
+	q1, err := createManagedQueue(root, "q1", false)
+	q1.CachedQueueInfo.GuaranteedResource = resources.NewResourceFromMap(
+		map[string]resources.Quantity{"memory": 0, "vcore": 0})
+	q1.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{
+		"memory": resources.Quantity(200),
+		"vcore" : resources.Quantity(200)})
+	if err != nil {
+		t.Fatalf("failed to create leaf queue: %v", err)
+	}
+
+	q2, err := createManagedQueue(root, "q2", false)
+	q2.CachedQueueInfo.GuaranteedResource = resources.NewResourceFromMap(map[string]resources.Quantity{
+		"memory": 0, "vcore": 0})
+	q2.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{
+		"memory": resources.Quantity(100),
+		"vcore" : resources.Quantity(100)})
+	if err != nil {
+		t.Fatalf("failed to create leaf queue: %v", err)
+	}
 
 	queues := []*SchedulingQueue{q0, q1, q2}
 	SortQueue(queues, FairSortPolicy)
 	assert.Equal(t, len(queues), 3)
-	assert.Equal(t, queues[0].Name, "q2")
-	assert.Equal(t, queues[1].Name, "q0")
-	assert.Equal(t, queues[2].Name, "q1")
+	assert.Equal(t, "root.q2", queues[0].Name)
+	assert.Equal(t, "root.q1", queues[1].Name)
+	assert.Equal(t, "root.q0", queues[2].Name)
 
-
-	q0.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{"memory": 200, "vcore": 100})
-	q1.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{"memory": 100, "vcore": 100})
+	q0.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{"memory": 200, "vcore": 200})
+	q1.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{"memory": 300, "vcore": 300})
 	SortQueue(queues, FairSortPolicy)
 	assert.Equal(t, len(queues), 3)
-	assert.Equal(t, queues[0].Name, "q2")
-	assert.Equal(t, queues[1].Name, "q1")
-	assert.Equal(t, queues[2].Name, "q0")
+	assert.Equal(t, "root.q2", queues[0].Name)
+	assert.Equal(t, "root.q0", queues[1].Name)
+	assert.Equal(t, "root.q1", queues[2].Name)
 }
 
-
-func newQueueInfo(name string, parent *cache.QueueInfo,
-	memoryMax, vcoreMax, memoryGuaranteed, vcoreGuaranteed int64) *cache.QueueInfo {
-	return &cache.QueueInfo{
-		Name:               name,
-		MaxResource:        resources.NewResourceFromMap(map[string]resources.Quantity{
-			"memory": resources.Quantity(memoryMax),
-			"vcore" : resources.Quantity(vcoreMax)}),
-		GuaranteedResource: resources.NewResourceFromMap(map[string]resources.Quantity{
-			"memory": resources.Quantity(memoryGuaranteed),
-			"vcore" : resources.Quantity(vcoreGuaranteed)}),
-		Parent:             parent,
-		Properties:         nil,
+// queue guaranteed resource is not set
+func TestQueueGuaranteedResourceNotSet(t *testing.T) {
+	root, err := createRootQueue()
+	if err != nil {
+		t.Fatalf("failed to create basic root queue: %v", err)
 	}
-}
+	assert.Nil(t, root.CachedQueueInfo.GuaranteedResource)
 
-func newSchedulingQueue(name string, queueInfo *cache.QueueInfo, parent *SchedulingQueue,
-	memoryProposed, vcoreProposed int64) *SchedulingQueue {
-	return &SchedulingQueue{
-		Name:                name,
-		CachedQueueInfo:     queueInfo,
-		ProposingResource:   resources.NewResourceFromMap(map[string]resources.Quantity{
-			"memory": resources.Quantity(memoryProposed),
-			"vcore" : resources.Quantity(vcoreProposed)}),
-		PartitionResource:   nil,
-		ApplicationSortType: 0,
-		QueueSortType:       FairSortPolicy,
-		childrenQueues:      nil,
-		applications:        nil,
-		parent:              parent,
-		allocatingResource:  nil,
-		pendingResource:     nil,
+	q0, err := createManagedQueue(root, "q0", false)
+	q0.CachedQueueInfo.GuaranteedResource = nil
+	q0.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{
+		"memory": resources.Quantity(300),
+		"vcore" : resources.Quantity(300)})
+	if err != nil {
+		t.Fatalf("failed to create leaf queue: %v", err)
 	}
+	assert.Nil(t, q0.CachedQueueInfo.GuaranteedResource)
+
+	q1, err := createManagedQueue(root, "q1", false)
+	q1.CachedQueueInfo.GuaranteedResource = nil
+	q1.ProposingResource = resources.NewResourceFromMap(map[string]resources.Quantity{
+		"memory": resources.Quantity(200),
+		"vcore" : resources.Quantity(200)})
+	if err != nil {
+		t.Fatalf("failed to create leaf queue: %v", err)
+	}
+	assert.Nil(t, q1.CachedQueueInfo.GuaranteedResource)
+
+	// q2 has no proposed resource
+	q2, err := createManagedQueue(root, "q2", false)
+	q2.CachedQueueInfo.GuaranteedResource = nil
+	if err != nil {
+		t.Fatalf("failed to create leaf queue: %v", err)
+	}
+	assert.Nil(t, q2.CachedQueueInfo.GuaranteedResource)
+
+	queues := []*SchedulingQueue{q0, q1, q2}
+	SortQueue(queues, FairSortPolicy)
+	assert.Equal(t, len(queues), 3)
+	assert.Equal(t, "root.q2", queues[0].Name)
+	assert.Equal(t, "root.q1", queues[1].Name)
+	assert.Equal(t, "root.q0", queues[2].Name)
 }

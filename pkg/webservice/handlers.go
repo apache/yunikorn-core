933139342ebd26a3243d6371b6242f8d57400efe
Adam Antal <adam.antal@cloudera.com>
2020-04-07 09:52:14 +0200

    Moving disabled metrics collection case upward

--- pkg/webservice/handlers.go
+++ pkg/webservice/handlers.go
@@ -241,24 +241,24 @@ func GetApplicationHistory(w http.ResponseWriter, r *http.Request) {
 	writeHeaders(w)
 
 	if imHistory != nil {
-		var result []*dao.ApplicationHistoryDAOInfo
-		records := imHistory.GetRecords()
-		for _, record := range records {
-			if record == nil {
-				continue
-			}
-			element := &dao.ApplicationHistoryDAOInfo{
-				Timestamp:         record.Timestamp.UnixNano(),
-				TotalApplications: strconv.Itoa(record.TotalApplications),
-			}
-			result = append(result, element)
-		}
-		if err := json.NewEncoder(w).Encode(result); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+		http.Error(w, "Internal metrics collection is not enabled.", http.StatusInternalServerError)
 		return
 	}
-	http.Error(w, "Internal metrics collection is not enabled.", http.StatusInternalServerError)
+	var result []*dao.ApplicationHistoryDAOInfo
+	records := imHistory.GetRecords()
+	for _, record := range records {
+		if record == nil {
+			continue
+		}
+		element := &dao.ApplicationHistoryDAOInfo{
+			Timestamp:         record.Timestamp.UnixNano(),
+			TotalApplications: strconv.Itoa(record.TotalApplications),
+		}
+		result = append(result, element)
+	}
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func GetContainerHistory(w http.ResponseWriter, r *http.Request) {

1f52dd38cf6ddf14d433ca567dc853baa82f4836
Adam Antal <adam.antal@cloudera.com>
2020-06-04 15:50:14 +0200

    Adding UT coverage for cache and store

--- pkg/events/event_cache_test.go
+++ pkg/events/event_cache_test.go
@@ -17,3 +17,96 @@
 */
 
 package events
+
+import (
+	"testing"
+	"time"
+
+	"github.com/apache/incubator-yunikorn-scheduler-interface/lib/go/si"
+	"gotest.tools/assert"
+)
+
+func TestSingleEvent(t *testing.T) {
+	cache := GetEventCache()
+	store := cache.GetEventStore()
+
+	cache.StartService()
+
+	ask := si.AllocationAsk{
+		AllocationKey: "alloc1",
+	}
+	event := baseEvent{
+		source:  &ask,
+		group:   "app1",
+		reason:  "reason",
+		message: "message",
+	}
+	cache.AddEvent(&event)
+
+	// wait for cache to process the event
+	time.Sleep(2 * sleepTimeInterval)
+
+	records := store.CollectEvents()
+	assert.Equal(t, len(records), 1)
+	record := records[0]
+	assert.Equal(t, record.Type, si.EventRecord_REQUEST)
+	assert.Equal(t, record.ObjectID, "alloc1")
+	assert.Equal(t, record.GroupID, "app1")
+	assert.Equal(t, record.Message, "message")
+	assert.Equal(t, record.Reason, "reason")
+}
+
+func TestMultipleEvents(t *testing.T) {
+	cache := GetEventCache()
+	store := cache.GetEventStore()
+
+	cache.StartService()
+
+	ask1 := si.AllocationAsk{
+		AllocationKey: "alloc1",
+	}
+	ask2 := si.AllocationAsk{
+		AllocationKey: "alloc2",
+	}
+	event1 := baseEvent{
+		source:  &ask1,
+		group:   "app1",
+		reason:  "reason1",
+		message: "message1",
+	}
+	event2 := baseEvent{
+		source:  &ask1,
+		group:   "app1",
+		reason:  "reason2",
+		message: "message2",
+	}
+	event3 := baseEvent{
+		source:  &ask2,
+		group:   "app2",
+		reason:  "reason3",
+		message: "message3",
+	}
+	cache.AddEvent(&event1)
+	cache.AddEvent(&event2)
+	cache.AddEvent(&event3)
+
+	// wait for cache to process the event
+	time.Sleep(2 * sleepTimeInterval)
+
+	records := store.CollectEvents()
+	assert.Equal(t, len(records), 2)
+	for _, record := range records {
+		assert.Equal(t, record.Type, si.EventRecord_REQUEST)
+		if record.ObjectID == "alloc1" {
+			assert.Equal(t, record.GroupID, "app1")
+			assert.Equal(t, record.Message, "message2")
+			assert.Equal(t, record.Reason, "reason2")
+		} else if record.ObjectID == "alloc2" {
+			assert.Equal(t, record.GroupID, "app2")
+			assert.Equal(t, record.Message, "message3")
+			assert.Equal(t, record.Reason, "reason3")
+		} else {
+			t.Fatalf("Unexpected allocation found")
+		}
+	}
+}
--- pkg/events/event_store_test.go
+++ pkg/events/event_store_test.go
@@ -17,3 +17,85 @@
 */
 
 package events
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+
+	"github.com/apache/incubator-yunikorn-scheduler-interface/lib/go/si"
+)
+
+func TestStoreAndRetrieveAllocationAsk(t *testing.T) {
+	store := newEventStoreImpl()
+	ask := si.AllocationAsk{
+		AllocationKey: "alloc1",
+	}
+	event := baseEvent{
+		source:  &ask,
+		group:   "app1",
+		reason:  "reason",
+		message: "message",
+	}
+	store.Store(&event)
+
+	records := store.CollectEvents()
+	assert.Equal(t, len(records), 1)
+	record := records[0]
+	assert.Equal(t, record.Type, si.EventRecord_REQUEST)
+	assert.Equal(t, record.ObjectID, "alloc1")
+	assert.Equal(t, record.GroupID, "app1")
+	assert.Equal(t, record.Message, "message")
+	assert.Equal(t, record.Reason, "reason")
+
+	// calling CollectEvents erases the events map
+	records = store.CollectEvents()
+	assert.Equal(t, len(records), 0)
+}
+
+func TestStoreAndRetrieveMultipleAllocationAsks(t *testing.T) {
+	store := newEventStoreImpl()
+	ask1 := si.AllocationAsk{
+		AllocationKey: "alloc1",
+	}
+	ask2 := si.AllocationAsk{
+		AllocationKey: "alloc2",
+	}
+	event1 := baseEvent{
+		source:  &ask1,
+		group:   "app1",
+		reason:  "reason1",
+		message: "message1",
+	}
+	event2 := baseEvent{
+		source:  &ask1,
+		group:   "app1",
+		reason:  "reason2",
+		message: "message2",
+	}
+	event3 := baseEvent{
+		source:  &ask2,
+		group:   "app2",
+		reason:  "reason3",
+		message: "message3",
+	}
+	store.Store(&event1)
+	store.Store(&event2)
+	store.Store(&event3)
+	records := store.CollectEvents()
+	assert.Equal(t, len(records), 2)
+	for _, record := range records {
+		assert.Equal(t, record.Type, si.EventRecord_REQUEST)
+		if record.ObjectID == "alloc1" {
+			assert.Equal(t, record.GroupID, "app1")
+			assert.Equal(t, record.Message, "message2")
+			assert.Equal(t, record.Reason, "reason2")
+		} else if record.ObjectID == "alloc2" {
+			assert.Equal(t, record.GroupID, "app2")
+			assert.Equal(t, record.Message, "message3")
+			assert.Equal(t, record.Reason, "reason3")
+		} else {
+			t.Fatalf("Unexpected allocation found")
+		}
+	}
+}

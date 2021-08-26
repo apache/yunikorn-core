bfa010588f2ab43714de5bbc869311913ad238a5
Weiwei Yang <wyang@cloudera.com>
2021-08-25 21:29:49 -0700

    remove unnecessary changes

--- pkg/scheduler/objects/node_test.go
+++ pkg/scheduler/objects/node_test.go
@@ -176,15 +176,6 @@ func TestPreAllocateCheck(t *testing.T) {
 	if err = node.preAllocateCheck(resSmall, "", false); err == nil {
 		t.Errorf("node with scheduling set to false should not allow allocation: %v", err)
 	}
-
-	// Check the DaemonSet case in non scheduling node
-	if err = node.preAllocateCheck(resSmall, "", false); err == nil {
-		t.Errorf("DaemonSet pod should schedule failed in reserved node: %v", err)
-	}
-	// clear the reservations in node
-	delete(node.reservations, reserve.getKey())
-	err = node.preAllocateCheck(resSmall, "", false)
-	assert.NilError(t, err, "DaemonSet pod should ignore the tag of Unschedulable node")
 }
 
 // Only test the CanAllocate code, the used logic in preAllocateCheck has its own test
@@ -203,12 +194,6 @@ func TestCanAllocate(t *testing.T) {
 	if node.CanAllocate(res, false) {
 		t.Error("node should have rejected allocation (oversize)")
 	}
-
-	// check if the resource request is from DaemosSet
-	if node.CanAllocate(res, false) {
-		t.Error("node should have rejected allocation (oversize)")
-	}
-
 	// check if preempting adds to available
 	node.IncPreemptingResource(resources.NewResourceFromMap(map[string]resources.Quantity{"first": 10}))
 	// preemption alloc

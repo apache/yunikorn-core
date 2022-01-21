55709ea56cab1d3a81b3fdc1038052d07545001f
anuraagnalluri <anuraagnalluri@gmail.com>
2022-01-21 00:05:49 -0800

    Location of the state dump file should be configurable

--- pkg/common/configs/config.go
+++ pkg/common/configs/config.go
@@ -46,12 +46,13 @@ type SchedulerConfig struct {
 // - a list of users specifying limits on the partition
 // - the preemption configuration for the partition
 type PartitionConfig struct {
-	Name           string
-	Queues         []QueueConfig
-	PlacementRules []PlacementRule           `yaml:",omitempty" json:",omitempty"`
-	Limits         []Limit                   `yaml:",omitempty" json:",omitempty"`
-	Preemption     PartitionPreemptionConfig `yaml:",omitempty" json:",omitempty"`
-	NodeSortPolicy NodeSortingPolicy         `yaml:",omitempty" json:",omitempty"`
+	Name              string
+	Queues            []QueueConfig
+	PlacementRules    []PlacementRule           `yaml:",omitempty" json:",omitempty"`
+	Limits            []Limit                   `yaml:",omitempty" json:",omitempty"`
+	Preemption        PartitionPreemptionConfig `yaml:",omitempty" json:",omitempty"`
+	NodeSortPolicy    NodeSortingPolicy         `yaml:",omitempty" json:",omitempty"`
+	StateDumpFilePath string                    `yaml:",omitempty" json:",omitempty"`
 }
 
 type PartitionPreemptionConfig struct {

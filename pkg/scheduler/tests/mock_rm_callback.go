5862209f9c34bbe1f34ad5ef252027b01802af27
manirajv06 <manirajv06@gmail.com>
2022-02-03 05:28:09 +0530

    [YUNIKORN-935] Refactor resourcemanagercallback mock classe (#369)
    Introduce a common mocked resource manager callback struct and reuse elsewhere in other tests.

---
+++ pkg/scheduler/tests/mock_rm_callback.go
@@ -0,0 +1,59 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package tests
+
+import (
+	"github.com/apache/incubator-yunikorn-scheduler-interface/lib/go/si"
+)
+
+type MockResourceManagerCallback struct{}
+
+func (f *MockResourceManagerCallback) UpdateApplication(response *si.ApplicationResponse) error {
+	return nil
+}
+
+func (f *MockResourceManagerCallback) UpdateAllocation(response *si.AllocationResponse) error {
+	return nil
+}
+
+func (f *MockResourceManagerCallback) UpdateNode(response *si.NodeResponse) error {
+	return nil
+}
+
+func (f *MockResourceManagerCallback) Predicates(args *si.PredicatesArgs) error {
+	// do nothing
+	return nil
+}
+
+func (f *MockResourceManagerCallback) ReSyncSchedulerCache(args *si.ReSyncSchedulerCacheArgs) error {
+	// do nothing
+	return nil
+}
+
+func (f *MockResourceManagerCallback) SendEvent(events []*si.EventRecord) {
+	// do nothing
+}
+
+func (f *MockResourceManagerCallback) UpdateConfiguration(args *si.UpdateConfigurationRequest) *si.UpdateConfigurationResponse {
+	// do nothing
+	return nil
+}
+
+func (f *MockResourceManagerCallback) UpdateContainerSchedulingState(request *si.UpdateContainerSchedulingStateRequest) {
+}

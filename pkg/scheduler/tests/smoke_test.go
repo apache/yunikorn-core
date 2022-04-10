d8692e1735144ea8642a835bdb870ead2734b84d
steinsgateted <steinsgateted@gmail.com>
2022-04-11 03:34:11 +0800

    [YUNIKORN-522] smoke test for dynamic queue clean up

--- pkg/scheduler/tests/smoke_test.go
+++ pkg/scheduler/tests/smoke_test.go
@@ -1448,7 +1448,7 @@ func TestDupReleasesInGangScheduling(t *testing.T) {
 	ms.mockRM.waitForAllocations(t, 1, 1000)
 }
 
-func TestDynamicQueueCleanUp(t *testing.T) {
+func TestQueueCleanUp(t *testing.T) {
 	configData := `
 partitions:
   - name: default

027c37b624757cbb60796baf23676896d47e47c7
0yukali0 <a08h0283@gmail.com>
2023-04-12 18:05:23 +0800

    [YUNIKORN-1669] Pass the node attributes to the webapp

--- pkg/webservice/handlers_test.go
+++ pkg/webservice/handlers_test.go
@@ -789,11 +789,12 @@ func TestGetPartitionNodes(t *testing.T) {
 	assert.NilError(t, err, "add application to partition should not have failed")
 
 	// create test nodes
+	attributesOfnodes := []map[string]string{map[string]string{"Disk": "SSD"}, map[string]string{"Devices": "camera"}}
 	nodeRes := resources.NewResourceFromMap(map[string]resources.Quantity{siCommon.Memory: 1000, siCommon.CPU: 1000}).ToProto()
 	node1ID := "node-1"
-	node1 := objects.NewNode(&si.NodeInfo{NodeID: node1ID, SchedulableResource: nodeRes})
+	node1 := objects.NewNode(&si.NodeInfo{NodeID: node1ID, Attributes: attributesOfnodes[0], SchedulableResource: nodeRes})
 	node2ID := "node-2"
-	node2 := objects.NewNode(&si.NodeInfo{NodeID: node2ID, SchedulableResource: nodeRes})
+	node2 := objects.NewNode(&si.NodeInfo{NodeID: node2ID, Attributes: attributesOfnodes[1], SchedulableResource: nodeRes})
 
 	// create test allocations
 	resAlloc1 := resources.NewResourceFromMap(map[string]resources.Quantity{siCommon.Memory: 500, siCommon.CPU: 300})
@@ -834,11 +835,13 @@ func TestGetPartitionNodes(t *testing.T) {
 			assert.Equal(t, node.NodeID, node1ID)
 			assert.Equal(t, "alloc-1", node.Allocations[0].AllocationKey)
 			assert.Equal(t, "alloc-1-uuid", node.Allocations[0].UUID)
+			assert.DeepEqual(t, attributesOfnodes[0], node.Attributes)
 			assert.DeepEqual(t, map[string]int64{"memory": 50, "vcore": 30}, node.Utilized)
 		} else {
 			assert.Equal(t, node.NodeID, node2ID)
 			assert.Equal(t, "alloc-2", node.Allocations[0].AllocationKey)
 			assert.Equal(t, "alloc-2-uuid", node.Allocations[0].UUID)
+			assert.DeepEqual(t, attributesOfnodes[1], node.Attributes)
 			assert.DeepEqual(t, map[string]int64{"memory": 30, "vcore": 50}, node.Utilized)
 		}
 	}

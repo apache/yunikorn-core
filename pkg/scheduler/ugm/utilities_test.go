a8821a0d36aaef8d8cdba387296696fa6ba3606d
Manikandan R <manirajv06@gmail.com>
2023-05-12 19:59:04 +0530

    Fixed import issues

--- pkg/scheduler/ugm/utilities_test.go
+++ pkg/scheduler/ugm/utilities_test.go
@@ -21,9 +21,10 @@ package ugm
 import (
 	"testing"
 
+	"gotest.tools/v3/assert"
+
 	"github.com/apache/yunikorn-core/pkg/common/resources"
 	"github.com/apache/yunikorn-core/pkg/webservice/dao"
-	"gotest.tools/v3/assert"
 )
 
 func internalGetResource(usage *dao.ResourceUsageDAOInfo, resources map[string]*resources.Resource) map[string]*resources.Resource {

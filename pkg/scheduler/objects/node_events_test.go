2e15ffe52ba3903205a0a1a3bb15cfa7335a60d7
Peter Bacsko <pbacsko@cloudera.com>
2023-06-23 11:29:31 +0200

    Coverage

--- pkg/scheduler/objects/node_events_test.go
+++ pkg/scheduler/objects/node_events_test.go
@@ -218,3 +218,38 @@ func TestSendCapacityChangedEvent(t *testing.T) {
 	protoRes := resources.NewResourceFromProto(event.Resource)
 	assert.DeepEqual(t, protoRes, resource)
 }
+
+func TestNodeSchedulableChangedEvent(t *testing.T) {
+	node := &Node{
+		NodeID:      nodeID1,
+		schedulable: true,
+	}
+
+	// not enabled
+	ne := newNodeEvents(node, nil)
+	ne.sendNodeSchedulableChangedEvent(false)
+
+	// enabled
+	mock := newEventSystemMock()
+	ne = newNodeEvents(node, mock)
+	ne.sendNodeSchedulableChangedEvent(false)
+	assert.Equal(t, 1, len(mock.events), "event was not generated")
+	event := mock.events[0]
+	assert.Equal(t, nodeID1, event.ObjectID)
+	assert.Equal(t, common.Empty, event.ReferenceID)
+	assert.Equal(t, "schedulable: false", event.Message)
+	assert.Equal(t, si.EventRecord_SET, event.EventChangeType)
+	assert.Equal(t, si.EventRecord_NODE_SCHEDULABLE, event.EventChangeDetail)
+	assert.Equal(t, 0, len(event.Resource.Resources))
+
+	mock.Reset()
+	ne.sendNodeSchedulableChangedEvent(true)
+	assert.Equal(t, 1, len(mock.events), "event was not generated")
+	event = mock.events[0]
+	assert.Equal(t, nodeID1, event.ObjectID)
+	assert.Equal(t, common.Empty, event.ReferenceID)
+	assert.Equal(t, "schedulable: true", event.Message)
+	assert.Equal(t, si.EventRecord_SET, event.EventChangeType)
+	assert.Equal(t, si.EventRecord_NODE_SCHEDULABLE, event.EventChangeDetail)
+	assert.Equal(t, 0, len(event.Resource.Resources))
+}

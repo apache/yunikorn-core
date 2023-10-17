80cc7b880e2eb02987a10ec83d4954b58f23c1d8
Yu-Lin Chen <kh87313@gmail.com>
2023-10-17 03:14:09 +0000

    Revert "[YUNIKORN-2025] Mismatched running container count if preemption was triggered"
    This reverts commit b519ce3c81434951b50e5bab0b9685d3e025889e.

--- pkg/rmproxy/rmproxy.go
+++ pkg/rmproxy/rmproxy.go
@@ -146,11 +146,7 @@ func (rmp *RMProxy) processRMReleaseAllocationEvent(event *rmevent.RMReleaseAllo
 			Released: event.ReleasedAllocations,
 		}
 		rmp.triggerUpdateAllocation(event.RmID, response)
-		// If preemption is triggered, we should prevent an increase in the count of released containers until shim detects it.
-		// When shim detects the release of a preempted allocation, the TerminationType will be set to STOPPED_BY_RM.
-		if event.ReleasedAllocations[0].TerminationType != si.TerminationType_PREEMPTED_BY_SCHEDULER {
-			metrics.GetSchedulerMetrics().AddReleasedContainers(len(event.ReleasedAllocations))
-		}
+		metrics.GetSchedulerMetrics().AddReleasedContainers(len(event.ReleasedAllocations))
 	}
 
 	// Done, notify channel

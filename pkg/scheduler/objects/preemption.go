c700283e660ac550cd29e61992eef97cabf68d3d
Manikandan R <manirajv06@gmail.com>
2024-07-05 15:08:09 +0530

    [YUNIKORN-2699] Preemption e2e tests fail in latest master

--- pkg/scheduler/objects/preemption.go
+++ pkg/scheduler/objects/preemption.go
@@ -641,7 +641,7 @@ func (p *Preemptor) TryPreemption() (*AllocationResult, bool) {
 	p.ask.MarkTriggeredPreemption()
 
 	// notify RM that victims should be released
-	p.application.notifyRMAllocationReleased(victims, si.TerminationType_PREEMPTED_BY_SCHEDULER,
+	p.application.notifyRMAllocationReleased(finalVictims, si.TerminationType_PREEMPTED_BY_SCHEDULER,
 		"preempting allocations to free up resources to run ask: "+p.ask.GetAllocationKey())
 
 	// reserve the selected node for the new allocation if it will fit

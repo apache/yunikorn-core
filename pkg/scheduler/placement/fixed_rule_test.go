7c4db98596b1c72fe8cf913be3e66ea16ff9d5e4
0lai0 <yuchenlai87@gmail.com>
2024-07-12 11:27:12 +0800

    [YUNIKORN2729] remove --new-from-rev from Makefile

--- pkg/scheduler/placement/fixed_rule_test.go
+++ pkg/scheduler/placement/fixed_rule_test.go
@@ -61,17 +61,7 @@ func TestFixedRule(t *testing.T) {
 }
 
 func TestFixedRulePlace(t *testing.T) {
-	// Create the structure for the test
-	const data = `
-partitions:
-  - name: default
-    queues:
-      - name: testqueue
-      - name: testparent
-        queues:
-          - name: testchild
-`
-	err := initQueueStructure([]byte(data))
+	err := initQueueStructure([]byte(confTestQueue))
 	assert.NilError(t, err, "setting up the queue config failed")
 
 	user := security.UserGroup{
